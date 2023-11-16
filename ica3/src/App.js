9a4ced2cdc5a4ddff63b0de86f72392d186bd0e2
mpoxson <mpoxson@gmail.com>
2023-11-16 11:08:02 -0500

    Test pages

--- ica3/src/App.js
+++ ica3/src/App.js
@@ -2,7 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>This is a test for main branch deploy</h1>
+    </div>
+  );
 }
 
 export default App;
